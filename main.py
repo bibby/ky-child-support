3b17d91f41bc85c4ff48c5a2bc86e73f6cde3237
bibby <bibby@bbby.org>
2024-03-22 16:22:30 -0400

    limit children

--- main.py
+++ main.py
@@ -31,6 +31,7 @@ if __name__ == '__main__':
         insurance=335
     )
 
+    children = min(children, 6)
     worksheet = Worksheet(children, him, her)
     print("\n")
     worksheet.calc_support()
